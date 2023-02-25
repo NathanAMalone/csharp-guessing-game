385805f9b24dac6e61422de895716d883ab410b1
Nathan Malone <nathanamalone@gmail.com>
2023-02-25 15:00:38 -0600

    Phase 1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+  
+         Console.WriteLine("Guess the secret number!");
+        
+        int NumberGuess = AsksForGuess("What is the secret number? "); 
+        Console.WriteLine(NumberGuess);
+        
+        int AsksForGuess(string question)
+            {
+                Console.Write($"{question}");
+                string answer = Console.ReadLine().ToLower();
+                int parsedAnswer = int.Parse(answer);
+
+                return parsedAnswer;
+
+                // while (answer != "y" && answer != "n")
+                // {
+                //     Console.Write($"{question} (Y/N): ");
+                //     answer = Console.ReadLine().ToLower();
+                // }
+
+                // if (answer == "y")
+                // {
+                //     return true;
+                // }
+                // else
+                // {
+                //     return false;
+                // }
+            }
+    
