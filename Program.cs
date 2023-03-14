7c47389d7483e06beea944302f71911f05318262
Nathan Malone <nathanamalone@gmail.com>
2023-03-14 18:01:34 -0500

    fixed four chances; added string to show user how many guesses left

--- Program.cs
+++ Program.cs
@@ -1,44 +1,41 @@
 ﻿using System;
   
-         Console.WriteLine("Guess the secret number!");
+        Console.WriteLine("Guess the secret number!");
         
         string NumberGuess = AsksForGuess("What is the secret number? "); 
         Console.WriteLine(NumberGuess);
         string AsksForGuess(string question)
             {
-                Console.Write($"{question}");
-                int secretNumber = 42;
-                string answer = Console.ReadLine().ToLower();
-                bool isNumber = int.TryParse(answer, out int parsedAnswer);
-                while (isNumber != true)
+                
+                    Console.Write($"{question} Guess (1) of (4): ");
+                    int secretNumber = 42;
+                    string answer = Console.ReadLine().ToLower();
+                    bool isNumber = int.TryParse(answer, out int parsedAnswer);
+                
+                for (int i = 0; i < 3; i++)
                     {
-                        Console.Write($"{question}");
-                        answer = Console.ReadLine().ToLower();
-                        isNumber = int.TryParse(answer, out parsedAnswer);
+                        if (isNumber != true)
+                            {
+                                Console.WriteLine("That is not a number!");
+                                Console.Write($"{question} Guess ({i + 2}) of (4): ");
+                                answer = Console.ReadLine().ToLower();
+                                isNumber = int.TryParse(answer, out parsedAnswer);
+                            }
+                        else {
+                        if (isNumber == true && parsedAnswer != secretNumber)
+                            {
+                                Console.WriteLine("That is the very wrong number!");
+                                Console.Write($"{question} Guess ({i + 2}) of (4): ");
+                                answer = Console.ReadLine().ToLower();
+                                isNumber = int.TryParse(answer, out parsedAnswer);
+                            }
+                        }    
                     }
-
-                if (parsedAnswer == secretNumber)
-                {
-                    return "42 is the secret number!";
-                }
-                else {
-                    return "That is the very wrong number!";
-                }
-                  
-
-                // while (answer != "y" && answer != "n")
-                // {
-                //     Console.Write($"{question} (Y/N): ");
-                //     answer = Console.ReadLine().ToLower();
-                // }
-
-                // if (answer == "y")
-                // {
-                //     return true;
-                // }
-                // else
-                // {
-                //     return false;
-                // }
+                    if (parsedAnswer == secretNumber)
+                    {
+                        return "42 is the secret number!";
+                    }
+                    else {
+                        return "That is the very wrong number, and you have no more guesses!";
+                    }
             }
-    
